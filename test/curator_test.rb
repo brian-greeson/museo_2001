80bf31c4f00a5fb7a9728d635028c6b22c12b6ce
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-04 09:28:38 -0700

    add add_photograph tests

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -10,9 +10,31 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_starts_with_no_photos
-    curator = Curator.new 
+    curator = Curator.new
     assert_equal [], curator.photographs
   end
 
+  def test_it_can_add_photos
+    curator = Curator.new
+
+    photo_1 = Photograph.new({
+                              id: "1",
+                              name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                              artist_id: "1",
+                              year: "1954"
+                            })
+
+    photo_2 = Photograph.new({
+                              id: "2",
+                              name: "Moonrise, Hernandez",
+                              artist_id: "2",
+                              year: "1941"
+                            })
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+
+    assert_equal [photo1, photo_2], curator.photographs
+  end
 
 end
