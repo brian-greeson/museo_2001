1ce11658ea8384e05ed5b0f3573144d5233784f7
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-04 09:19:55 -0700

    Pass attributes test

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,14 @@
 class Artist
-
+  attr_reader :id, :name, :born, :died, :country, :age_at_death
+  
   def initialize(attributes)
-    @attributes = attributes
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+    @age_at_death = @died.to_i - @born.to_i
   end
+
 
 end
