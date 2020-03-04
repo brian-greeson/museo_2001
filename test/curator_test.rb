f376cd5710f5424e0fc0dc973fa3d3d0b3e18a1c
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-04 09:47:10 -0700

    Add photos by artist test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -90,4 +90,86 @@ class CuratorTest < Minitest::Test
     assert_nil curator.find_artist_by_id("3")
   end
 
+  def test_it_can_list_photos_by_artist
+    curator = Curator.new
+    photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    photo_3 = Photograph.new({
+         id: "3",
+         name: "Identical Twins, Roselle, New Jersey",
+         artist_id: "3",
+         year: "1967"
+    })
+    photo_4 = Photograph.new({
+         id: "4",
+         name: "Monolith, The Face of Half Dome",
+         artist_id: "3",
+         year: "1927"
+    })
+    artist_1 = Artist.new({
+         id: "1",
+         name: "Henri Cartier-Bresson",
+         born: "1908",
+         died: "2004",
+         country: "France"
+    })
+    artist_2 = Artist.new({
+         id: "2",
+         name: "Ansel Adams",
+         born: "1902",
+         died: "1984",
+         country: "United States"
+    })
+    artist_3 = Artist.new({
+         id: "3",
+         name: "Diane Arbus",
+         born: "1923",
+         died: "1971",
+         country: "United States"
+    })
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    expected = {
+                 artist_1 => [photo_1],
+                 artist_2 => [photo_2],
+                 artist_3 => [photo_3,photo_4]
+               }
+
+    assert_equal expected, curator.photographs_by_artist
+  end
+
+
 end
+
+# 
+# curator.photographs_by_artist
+# # => {
+# #        #<Artist:1...> => [#<Photograph:1...>],
+# #        #<Artist:2...> => [#<Photograph:2...>],
+# #        #<Artist:3...> => [#<Photograph:3...>, #<Photograph:4...>]
+# #      }
+#
+# curator.artists_with_multiple_photographs
+# # => ["Diane Arbus"]
+#
+# curator.photographs_taken_by_artist_from("United States")
+# # => [#<Photograph:2...>, #<Photograph:3...>, #<Photograph:4...>
+#
+# curator.photographs_taken_by_artist_from("Argentina")
+# => []
