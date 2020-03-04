08334a20514e0c381480f414e5f144ff31574c78
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-04 10:16:11 -0700

    Add load photos from file test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -272,4 +272,12 @@ class CuratorTest < Minitest::Test
     assert_equal expected, curator.photographs_taken_by_artist_from("United States")
     assert_equal [], curator.photographs_taken_by_artist_from("Argentina")
   end
+
+  def test_it_can_load_photos_from_file
+    curator = Curator.new
+    curator.load_photographs('./data/photographs.csv')
+
+    assert_equal 4, curator.photographs.length
+    assert_equal "Moonrise, Hernandez", curator.photographs[1].name
+  end
 end
